9a5f937d98ddcadf558a2bac7d3b22a2d4689f92
Doraeki <idh0325@naver.com>
2022-09-27 17:48:06 +0900

    스프링부트 프로젝트 셋팅

---
+++ src/main/java/com/study/mvc20220927dohyun/Mvc20220927DohyunApplication.java
@@ -0,0 +1,13 @@
+package com.study.mvc20220927dohyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc20220927DohyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc20220927DohyunApplication.class, args);
+	}
+
+}
